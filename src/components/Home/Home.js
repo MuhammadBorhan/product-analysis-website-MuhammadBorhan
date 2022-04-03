37a8cad31b73d5af8ba3ab9d373e53ea0e4baac5
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-03 23:58:04 +0600

    home top done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../Home/rolex.jpeg';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className='grid grid-cols-4 px-16 items-center'>
+            <div className='col-span-3'>
+                <h2 className='text-5xl font-bold'>This is best watch</h2>
+                <h2 className='text-5xl font-bold text-blue-500 mt-2'>What are you looking for</h2>
+                <p className='w-3/4 mt-6 text-xl'>This is your best option for buying this watch. This is your best option for buying this watch. This is your best option for buying this watch.</p>
+            </div>
+            <div>
+                <img className='rounded-lg' src={logo} alt="" />
+            </div>
         </div>
     );
 };
