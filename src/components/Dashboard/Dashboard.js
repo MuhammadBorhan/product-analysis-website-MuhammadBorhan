7b93ae810a3e98bf4e3817f00588ac2cd57d03af
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-04 14:44:30 +0600

    customer Review done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const data = [
@@ -12,13 +12,13 @@ const Dashboard = () => {
         {
             "month": "Apr",
             "investment": 200000,
-            "sell": 555423,
+            "sell": 455423,
             "revenue": 324500
         },
         {
             "month": "May",
             "investment": 500000,
-            "sell": 45726,
+            "sell": 145726,
             "revenue": 567010
         },
         {
@@ -40,14 +40,43 @@ const Dashboard = () => {
             "revenue": 361000
         }
     ];
+
+    const pieData1 = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+    ];
+    const pieData2 = [
+        { name: 'A1', value: 100 },
+        { name: 'A2', value: 300 },
+        { name: 'B1', value: 100 },
+        { name: 'B2', value: 80 },
+        { name: 'B3', value: 40 },
+        { name: 'B4', value: 30 },
+        { name: 'B5', value: 50 },
+        { name: 'C1', value: 100 },
+        { name: 'C2', value: 200 },
+        { name: 'D1', value: 150 },
+        { name: 'D2', value: 50 },
+    ];
     return (
-        <div>
-            <LineChart width={500} height={500} data={data}>
-                <Line dataKey={'revenue'}></Line>
-                <Line dataKey={'sell'}></Line>
-                <XAxis dataKey={'month'}></XAxis>
-                <YAxis dataKey={'investment'}></YAxis>
-            </LineChart>
+        <div className='grid grid-cols-2 px-16 py-8'>
+            <div>
+                <LineChart width={500} height={500} data={data}>
+                    <Line dataKey={'revenue'}></Line>
+                    <Line dataKey={'sell'}></Line>
+                    <XAxis dataKey={'month'}></XAxis>
+                    <YAxis dataKey={'investment'}></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <div className='mx-auto'>
+                <PieChart width={400} height={400}>
+                    <Pie data={pieData1} dataKey="value" cx="50%" cy="50%" outerRadius={70} fill="#8884d8" />
+                    <Pie data={pieData2} dataKey="value" cx="50%" cy="50%" innerRadius={80} outerRadius={160} fill="#82ca9d" label />
+                </PieChart>
+            </div>
         </div>
     );
 };
