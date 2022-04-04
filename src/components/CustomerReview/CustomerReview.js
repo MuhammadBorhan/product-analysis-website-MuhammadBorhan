f47a817799c87d533716fc730fc6559121fa58b6
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-04 19:30:52 +0600

    rating icon done

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const CustomerReview = ({ review }) => {
     const { name, comment, rating } = review;
@@ -6,7 +8,7 @@ const CustomerReview = ({ review }) => {
         <div className='bg-green-700 m-2 p-2 rounded text-white'>
             <h1>Name: {name}</h1>
             <h4 className='my-2'>Comment: <span className='text-yellow-500'>{comment}</span></h4>
-            <p>Rating: <span className='text-orange-300 font-bold'>{rating}</span></p>
+            <p>Rating: <span className='text-orange-300 font-bold'>{rating} <FontAwesomeIcon icon={faStar}></FontAwesomeIcon> </span></p>
         </div>
     );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Review = ({ review }) => {
     const { name, comment, rating } = review;
@@ -6,7 +8,7 @@ const Review = ({ review }) => {
         <div className='bg-green-700 text-white m-2 p-2 rounded'>
             <h1>Name: <span>{name}</span></h1>
             <h4 className='my-2'>Comment: <span className='text-yellow-500'>{comment}</span></h4>
-            <p>Rating: <span className='text-orange-300 font-bold'>{rating}</span></p>
+            <p>Rating: <span className='text-orange-300 font-bold'>{rating} <FontAwesomeIcon icon={faStar}></FontAwesomeIcon> </span></p>
         </div>
     );
 };
